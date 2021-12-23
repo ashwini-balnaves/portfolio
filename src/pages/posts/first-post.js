faf17661936bc681b4785bb1e3e2d5eb55bab46c
Ashwini Balnaves <ashwini.balnaves@kapiche.com>
2021-12-23 18:50:33 +1000

    wip

---
+++ src/pages/posts/first-post.js
@@ -0,0 +1,28 @@
+import Script from 'next/script'
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+
+export default function FirstPost() {
+    return (
+      <Layout>
+        <Head>
+          <title>Hello Celeste!</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1>Hello Celeste!</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </Layout>
+    )
+  }
