807eaf8fd8ea4cf5d6e212bcdc93490006b316c2
ashwini <ashwini.balnaves@kapiche.com>
2022-04-25 19:04:47 +1000

    moves footer to prepare for navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,94 +2,73 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 
+import Footer from "./footer"
+
 const name = "Ashwini Balnaves";
 export const siteTitle = "Ashwini Balnaves · portfolio";
-const linkedInUrl = "https://www.linkedin.com/in/ashwini-balnaves/";
-const githubUrl = "https://github.com/ashwini-balnaves/";
-const twitterUrl = "https://twitter.com/that_ashwini";
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="The personal website of Ashwini Balnaves"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profilebig.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            <p className={utilStyles.subtitle}> Senior, full stack software engineer &middot; Brisbane, QLD</p>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profilebig.jpg"
-                  className={utilStyles.borderCircle}
-                  height={100}
-                  width={100}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-      <footer className={styles.footer}>
-
-        <a href={`${linkedInUrl}`} target="_blank">
-          <FontAwesomeIcon icon={faLinkedin}
-            className={`${styles.fa} ${styles.faLinkedin}`}
+    <div className={styles.appWrapper}>
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="The personal website of Ashwini Balnaves"
           />
-        </a>
-        <a href={`${githubUrl}`} target="_blank">
-          <FontAwesomeIcon icon={faGithub} className={`${styles.fa} ${styles.faGithub}`} />
-        </a>
-        <a href={`${twitterUrl}`} target="_blank">
-          <FontAwesomeIcon icon={faTwitter} className={`${styles.fa} ${styles.faTwitter}`} />
-        </a>
-      </footer>
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <Image
+                priority
+                src="/images/profilebig.jpg"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={144}
+                alt={name}
+              />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+              <p className={utilStyles.subtitle}> Senior, full stack software engineer &middot; Brisbane, QLD</p>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <a>
+                  <Image
+                    priority
+                    src="/images/profilebig.jpg"
+                    className={utilStyles.borderCircle}
+                    height={100}
+                    width={100}
+                    alt={name}
+                  />
+                </a>
+              </Link>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/">
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+      </div>
+      <Footer />
     </div>
   );
 }
